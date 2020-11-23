be34f4dede2fd878fea96c5fdcae345c6f2a728e
Yrom <igor2.004@mail.ru>
2020-11-23 16:09:56 +0300

    coursework

--- курсач/1.js
+++ курсач/1.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	$('.headrburger').click(function(event){
 		$('.headrburger,.headrmenu, .contents').toggleClass('active');
 		$('body').toggleClass('lock');
+		window.scrollTo({ top: 0, behavior: 'smooth' });
 	});
 });
